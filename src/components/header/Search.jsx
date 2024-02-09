1dfa728035d33744bdb0c575009fa69f487b8f9b
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-02-09 16:27:46 +0600

    delay update

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -5,34 +5,34 @@ import { getLocationByName } from "../../data/location-data";
 import { useDebounce } from "../../hooks";
 
 function Search() {
-    const { setSelectedLocation } = useContext(LocationContext);
+  const { setSelectedLocation } = useContext(LocationContext);
 
-    const doSearch = useDebounce((term) => {
-        const fetchedLocation = getLocationByName(term);
-        setSelectedLocation({ ...fetchedLocation });
-    }, 500);
+  const doSearch = useDebounce((term) => {
+    const fetchedLocation = getLocationByName(term);
+    setSelectedLocation({ ...fetchedLocation });
+  }, 1000);
 
-    function handleChange(e) {
-        const value = e.target.value;
-        doSearch(value);
-    }
+  function handleChange(e) {
+    const value = e.target.value;
+    doSearch(value);
+  }
 
-    return (
-        <form action="#">
-            <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
-                <input
-                    className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
-                    type="search"
-                    placeholder="Search Location"
-                    onChange={handleChange}
-                    required
-                />
-                <button type="submit">
-                    <img src={SearchIcon} alt="Search" />
-                </button>
-            </div>
-        </form>
-    );
+  return (
+    <form action="#">
+      <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
+        <input
+          className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
+          type="search"
+          placeholder="Search Location"
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">
+          <img src={SearchIcon} alt="Search" />
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default Search;
